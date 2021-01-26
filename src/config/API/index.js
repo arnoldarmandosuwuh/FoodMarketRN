4851b17621cd74b7d22d24c8add29c2771d18107
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2021-01-27 00:12:56 +0700

    update url

--- src/config/API/index.js
+++ src/config/API/index.js
@@ -1,9 +1,4 @@
-// export const API_HOST = {
-//   url: 'http://192.168.100.8:8000/api',
-//   storage: 'http://192.168.100.8:8000/storage',
-// }
-
 export const API_HOST = {
-  url: 'https://foodmarket-backend.buildwithangga.id/api',
-  storage: 'https://foodmarket-backend.buildwithangga.id/storage',
+  url: 'http://foodmarket-backend.buildwithangga.id/api',
+  storage: 'http://foodmarket-backend.buildwithangga.id/storage',
 }
